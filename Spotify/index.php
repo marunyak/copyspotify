f9df53728c4ccd360cfbae6dbff6239d25c4245d
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-05-31 00:12:53 +0300

    Was delete navBarContainer

--- Spotify/index.php
+++ Spotify/index.php
@@ -23,30 +23,7 @@
 <body>
 <div id="mainContainer">
 	<div id="topContainer">
-		<div id="navBarContainer">
-			<nav class="navBar">
-				<a href="index.php" class="logo">
-					<img src="spotify-logo (1).png">
-				</a>
-				<div class="group">
-					<div class="navItem">
-						<a href="search.php" class="navItemLink">Search</a>
-					</div>
-				</div>
-
-				<div class="group">
-					<div class="navItem">
-						<a href="browse.php" class="navItemLink">Browse</a>
-					</div>
-					<div class="navItem">
-						<a href="yourMusic.php" class="navItemLink">Your Music</a>
-					</div>
-					<div class="navItem">
-						<a href="profile.php" class="navItemLink">Andrew M</a>
-					</div>
-				</div>
-			</nav>
-		</div>
+		
 	</div>
 
 	<div id="nowPlayingBarContainer">
