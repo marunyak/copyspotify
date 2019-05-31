c9562612dea080f33ce70e8cdca11910f102c192
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-05-31 23:35:10 +0300

    mainContainer,mainViewContainer

--- Spotify/index.php
+++ Spotify/index.php
@@ -24,6 +24,12 @@
 <div id="mainContainer">
 	<div id="topContainer">
 		<?php include "includes/navBarContainer.php";?>
+
+		<div id="mainViewContainer">
+			<div id="mainContent">
+
+			</div>
+		</div>
 	</div>
 	<?php include "includes/nowPlayingBarContainer.php";?>
 </div>
