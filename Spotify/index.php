a17c6b10ec6c4cf92a2f69b1a8a52cb2d86d621a
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-05-31 23:51:41 +0300

    added header.php

--- Spotify/index.php
+++ Spotify/index.php
@@ -1,32 +1,4 @@
-<?php
-	include("includes/config.php");
-
-	//session_destroy();
-
-	if(isset($_SESSION['userLoggedIn'])){
-		$userLoggedIn = $_SESSION['userLoggedIn'];
-	}
-	else{
-		header("Location: register.php");
-	}
-
-
- ?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Welcome to Spotify!</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-<div id="mainContainer">
-	<div id="topContainer">
-		<?php include "includes/navBarContainer.php";?>
-
-		<div id="mainViewContainer">
-			<div id="mainContent">
+	<?php include "includes/header.php";?>
 
 			</div>
 		</div>
