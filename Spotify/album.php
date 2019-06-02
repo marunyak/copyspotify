8ba47e2933d7f3233f2a2999d79948d16e483d05
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-06-02 23:04:16 +0300

    Changed album.php

--- Spotify/album.php
+++ Spotify/album.php
@@ -9,7 +9,13 @@ else {
 
 $albumQuery = mysqli_query($con,"SELECT * FROM albums WHERE id='{$albumId}'");
 $album = mysqli_fetch_array($albumQuery);
+$artistId =  $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id ='{$artistId}'");
+$artist = mysqli_fetch_array($artistQuery);
+
 echo $album['title'];
+echo $artist['name'];
 ?>
 
 <?php include "includes/footer.php";?>
