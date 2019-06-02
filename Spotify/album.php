f271a5f2e97645bc63e32f691f422f4d687991de
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-06-02 22:50:18 +0300

    Added album.php

---
+++ Spotify/album.php
@@ -0,0 +1,15 @@
+<?php include "includes/header.php";
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $albumId = $_GET['id'];
+}
+else {
+    header("Location: index.php");
+}
+
+$albumQuery = mysqli_query($con,"SELECT * FROM albums WHERE id='{$albumId}'");
+$album = mysqli_fetch_array($albumQuery);
+echo $album['title'];
+?>
+
+<?php include "includes/footer.php";?>
