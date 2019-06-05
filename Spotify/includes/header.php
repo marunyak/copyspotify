8b66b88e55bd2982d77cf12ceda9166974b253b0
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-06-06 00:56:56 +0300

    Added class Audio

--- Spotify/includes/header.php
+++ Spotify/includes/header.php
@@ -21,8 +21,28 @@
 <head>
 	<title>Welcome to Spotify!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script src="assets/js/script.js"></script>
 </head>
 <body>
+<script>
+	var audioElement = new Audio();
+	audioElement.setTrack('assets/music/thank_u_next.mp3');
+	var playPromise = audioElement.audio.play();
+
+	// if (playPromise !== undefined) {
+	// 	playPromise.then(_ => {
+	// 	// Automatic playback started!
+	// 	// Show playing UI.
+	// 	audioElement.audio.play();
+	// 	})
+	// 	.catch(error => {
+	// 	// Auto-play was prevented
+	// 	// Show paused UI.
+	// 	audioElement.audio.pause();
+	// 	});
+	// }
+
+</script>
 <div id="mainContainer">
 	<div id="topContainer">
 		<?php include "includes/navBarContainer.php";?>
