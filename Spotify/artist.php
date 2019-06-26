c1864b0ae5ea8adf20f1fe47f212f37cf1c4527c
Andrey Marunyak <marunyak.andrey@gmail.com>
2019-06-26 18:54:45 +0300

    Finised added styles for browse.php

---
+++ Spotify/artist.php
@@ -0,0 +1,82 @@
+<?php 
+include("includes/includedFiles.php");
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $artistId = $_GET['id'];
+}
+else {
+    header("Location: index.php");
+}
+
+$artist = new Artist($con, $artistId);
+?>
+<div class="entityInfo borderBottom">
+    <div class="centerSection">
+        <div class="artistInfo">
+            <h1 class="artistName" ><?php echo $artist->getName();?></h1>
+            <div class="headerButtons">
+                <button class="button green">PLAY</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="tracklistContainer borderBottom">
+<h2>POPULAR</h2>
+    <ul class="tracklist">
+        <?php
+            $songIdArray = $artist->getSongIds();
+            $i = 1;
+            foreach($songIdArray as $songId){
+
+                if($i > 5) break;
+
+                $albumSong = new Song($con,$songId);
+                $albumArtist = $albumSong->getArtist();
+
+                echo "
+                    <li class='tracklistRow'>
+                        <div class='trackCount'>
+                            <img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"".$albumSong->getId()."\",tempPlayList,true)'>
+                            <span class='trackNumber'>{$i}</span>
+                        </div>
+
+                        <div class='trackInfo'>
+                            <span class='trackName'>{$albumSong->getTitle()}</span>
+                            <span class='artistName'>{$albumArtist->getName()}</span>
+                        </div>
+
+                        <div class='trackOptions'>
+                            <img class='optionsButton' src='assets/images/icons/more.png'>
+                        </div>
+
+                        <div class='trackDuration'>
+                            <span class='duration'>".$albumSong->getDuration()."</span>
+                        </div>
+                    </li>
+                ";
+                $i++;
+            }
+        ?>
+
+        <script>
+            var tempPlayList = <?php echo json_encode($songIdArray);?>;
+        </script>
+    </ul>
+</div>
+
+<div class="gridViewContainer">
+<h2>ALBUMS</h2>
+    <?php
+        $albumQuery = mysqli_query($con,"SELECT * FROM albums WHERE artist = '{$artistId}' LIMIT 10");
+        while($row = mysqli_fetch_array($albumQuery)){
+            echo "
+            <div class='gridViewItem'>
+                <a  role='link' tabindex='0' onclick='openPage(\"album.php?id=".$row['id']."\")' >
+                    <img src='".$row['artworkPath']."'>
+                    <div class='gridViewInfo'>".$row['title']."</div>
+                </a>
+            </div>";
+        }
+    ?>
+</div>
